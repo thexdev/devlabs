243b34172188fe3625eef904f702210d9d4f493a
M. Akbar Nugroho <akbarnugroho253@gmail.com>
2025-04-12 23:03:17 +0700

    test(go-hex-arch): fix directory path

--- go-hex-arch/internal/infra/secondary/repository/inmemory_repository_test.go
+++ go-hex-arch/internal/infra/secondary/repository/inmemory_repository_test.go
@@ -1,7 +1,7 @@
 package repository_test
 
 import (
-	"gohexarch/internal/infra/primary/secondary/repository"
+	"gohexarch/internal/infra/secondary/repository"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
